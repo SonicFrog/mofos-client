30e2e0f84a92a55d7fa761336df6c02c58743e0d
Ogier Bouvier <ars3nic@ars3nic.ch>
2017-09-27 10:27:17 +0200

    tup: add build script

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+readonly CONFIG_DIR=./config
+
+[[ -d .tup ]] || tup init
+
+for i in $@; do
+	case "$i" in
+		test)
+			tup variant $CONFIG_DIR/test.config
+			tup
+			;;
+
+		release)
+			tup variant $CONFIG_DIR/release.config
+			tup
+			;;
+
+		clean)
+			rm -rf build-*
+			;;
+
+		*)
+			printf "Invalid argument $i\n"
+			exit 1
+			;;
+	esac
+done
